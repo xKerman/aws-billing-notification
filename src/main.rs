208630bfcd61b6e395e77d93b4095126d3b131f3
xKhorasan <xKhorasan@gmail.com>
2018-12-07 02:59:41 +0900

    fetch billing information from CloudWatch

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ extern crate serde_derive;
 extern crate log;
 extern crate simple_logger;
 extern crate rusoto_core;
+extern crate chrono;
 
 use std::error::Error;
 
+use chrono::{Duration, SecondsFormat, Utc};
 use lambda::error::HandlerError;
 use rusoto_core::Region;
-use rusoto_cloudwatch::CloudWatchClient;
+use rusoto_cloudwatch::{CloudWatch, CloudWatchClient, Dimension, GetMetricStatisticsInput};
 
 #[derive(Deserialize, Clone)]
 struct CustomEvent {
@@ -22,6 +24,7 @@ struct CustomEvent {
 #[derive(Serialize, Clone)]
 struct CustomOutput {
     message: String,
+    total: f64,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -33,7 +36,33 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 
 fn my_handler(e: CustomEvent, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
+    let duration = Duration::days(1);
+    let end_time = Utc::now();
+    let start_time = end_time - duration;
     let client = CloudWatchClient::new(Region::UsEast1);
+    let metric = client.get_metric_statistics(GetMetricStatisticsInput {
+        dimensions: Some(vec![Dimension {
+            name: "Currency".to_string(),
+            value: "USD".to_string(),
+        }]),
+        metric_name: "EstimatedCharges".to_string(),
+        namespace: "AWS/Billing".to_string(),
+        statistics: Some(vec!["Maximum".to_string()]),
+        start_time: start_time.to_rfc3339_opts(SecondsFormat::Secs, true),
+        end_time: end_time.to_rfc3339_opts(SecondsFormat::Secs, true),
+        period: duration.num_seconds(),
+        extended_statistics: None,
+        unit: None,
+    });
+    let total = match metric.sync() {
+        Err(err) => return Err(c.new_error(&err.to_string())),
+        Ok(metric) => metric.datapoints.map(|dp| {
+            if dp.is_empty() {
+                return 0.0;
+            }
+            return dp[0].maximum.unwrap_or(0.0);
+        }).unwrap_or(0.0),
+    };
     if e.first_name == "" {
         error!("Empty first name in request {}", c.aws_request_id);
         return Err(c.new_error("Empty first name"));
@@ -41,5 +70,6 @@ fn my_handler(e: CustomEvent, c: lambda::Context) -> Result<CustomOutput, Handle
 
     Ok(CustomOutput {
         message: format!("Hello, {}!", e.first_name),
+        total: total,
     })
 }
